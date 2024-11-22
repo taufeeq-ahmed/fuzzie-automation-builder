1fa2598f66f61435be388b20e480bb756e35f6f0
taufeeq-ahmed <io.taufeeq.ahmed@gmail.com>
2024-11-22 19:08:00 +0530

    workflow is implmented

--- src/app/(main)/(pages)/workflows/_components/workflows.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflows.tsx
@@ -1,21 +1,30 @@
 import React from "react";
-// import Workflow from "./workflow";
+import Workflow from "./workflow";
 // import { onGetWorkflows } from "../_actions/workflow-connections";
 // import MoreCredits from "./more-creadits";
 
 const Workflows = async () => {
-  //   const workflows = await onGetWorkflows();
+  // const workflows = await onGetWorkflows();
+  const workflows = [
+    {
+      name: "Automation Testing",
+      description: "string",
+      id: "string",
+      publish: true,
+    },
+  ];
+
   return (
     <div className="relative flex flex-col gap-4">
-      <section className="flex flex-col m-2">
+      <section className="flex flex-col m-2 p-4">
         {/* <MoreCredits /> */}
-        {/* {workflows?.length ? (
+        {workflows?.length ? (
           workflows.map((flow) => <Workflow key={flow.id} {...flow} />)
         ) : (
           <div className="mt-28 flex text-muted-foreground items-center justify-center">
             No Workflows
           </div>
-        )} */}
+        )}
       </section>
     </div>
   );
--- src/app/(main)/(pages)/workflows/page.tsx
+++ src/app/(main)/(pages)/workflows/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import WorkflowButton from "./_components/workflow-button";
+import Workflows from "./_components/workflows";
 
 const Page = () => {
   return (
@@ -8,7 +9,7 @@ const Page = () => {
         Workflows
         <WorkflowButton />
       </h1>
-      {/* <Workflows /> */}
+      <Workflows />
     </div>
   );
 };
